ef0a7926defd361cf53e32a5e91bea0248a757e2
AndyFranck23 <151866105+AndyFranck23@users.noreply.github.com>
2025-04-01 15:56:58 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-        return [
-            {
-                // Toutes les URL de n'importe quel chemin
-                source: '/:path*',
-                // Condition sur l'hôte (disponible sur Vercel et dans certaines configurations)
-                has: [
-                    {
-                        type: 'host',
-                        value: 'botia.ai',
-                    },
-                ],
-                // Redirection vers la version avec www en conservant le chemin et les paramètres
-                destination: 'https://www.botia.ai/:path*',
-                permanent: true,
-            },
-        ];
-    },
-    webpack: (config) => {
-        config.resolve.fallback = { fs: false };
-        return config;
-    }
 };
 
 export default nextConfig;
