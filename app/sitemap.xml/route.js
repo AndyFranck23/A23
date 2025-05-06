c795c283dcec5275bb985d042848448199c1688f
AndyFranck23 <ironmanandy23@gmail.com>
2025-05-06 12:41:35 +0300

    sitemap3

--- app/sitemap.xml/route.js
+++ app/sitemap.xml/route.js
@@ -28,13 +28,13 @@ async function getSitemapData() {
                 url: `${process.env.NEXT_PUBLIC_SITE_URL}/`,
                 lastModified: new Date().toISOString(),
                 changeFrequency: 'daily',
-                // priority: 1.0,
+                priority: 1.0,
             },
             {
                 url: `${process.env.NEXT_PUBLIC_SITE_URL}/about`,
                 lastModified: new Date().toISOString(),
                 changeFrequency: 'daily',
-                // priority: 0.6,
+                priority: 0.6,
             }
         ];
 
@@ -42,35 +42,35 @@ async function getSitemapData() {
             url: `${process.env.NEXT_PUBLIC_SITE_URL}/${item.slug}`,
             lastModified: new Date().toISOString(),
             changeFrequency: 'daily',
-            // priority: 0.2,
+            priority: 0.2,
         })) || [];
 
         const dynamicPagesClassement = categories?.map((item) => ({
             url: `${process.env.NEXT_PUBLIC_SITE_URL}/${item.produit.slug}/${item.slug}`,
             lastModified: new Date().toISOString(),
             changeFrequency: 'daily',
-            // priority: 0.3,
+            priority: 0.3,
         })) || [];
 
         const dynamicPagesOffres = offres?.map((item) => ({
             url: `${process.env.NEXT_PUBLIC_SITE_URL}/${item.produit.slug}/${item.categorie.slug}/${item.slug}`,
             lastModified: new Date().toISOString(),
             changeFrequency: 'daily',
-            // priority: 0.4,
+            priority: 0.4,
         })) || [];
 
         const dynamicPagesBlog = articles?.map((item) => ({
             url: `${process.env.NEXT_PUBLIC_SITE_URL}/blog/${item.slug}`,
             lastModified: new Date().toISOString(),
             changeFrequency: 'daily',
-            // priority: 0.5,
+            priority: 0.5,
         })) || [];
 
         const dynamicPagesPages = pages?.map((item) => ({
             url: `${process.env.NEXT_PUBLIC_SITE_URL}/page/${item.slug}`,
             lastModified: new Date().toISOString(),
             changeFrequency: 'daily',
-            // priority: 0.7,
+            priority: 0.7,
         })) || [];
 
         return [
@@ -98,7 +98,7 @@ export async function GET() {
     <loc>${url.url}</loc>
     <lastmod>${url.lastModified}</lastmod>
     <changefreq>${url.changeFrequency}</changefreq>
-
+    <priority>${url.priority}</priority>
   </url>`
             )
             .join('')}
@@ -110,4 +110,3 @@ export async function GET() {
         },
     });
 }
-{/* <priority>${url.priority}</priority> */ }
