cae3286f89860347200dfca6dfc1caf3122f83f2
AndyFranck23 <ironmanandy23@gmail.com>
2025-06-30 10:20:29 +0300

    version 0.1.0s7 robot.txt

--- app/robots.js
+++ app/robots.js
@@ -6,6 +6,6 @@ export default function robots() {
             // Pour bloquer certains dossiers, par exemple :
             disallow: ['/admin/', '/login/', '/signup/']
         },
-        sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`
+        sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap-new.xml`
     }
 }
